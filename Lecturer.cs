36c413d2627eba981b02e26f9ab7124f79c97bb4
degruendel <de.gruendel@gmail.com>
2019-02-05 15:50:31 +0100

    added students and lecturer in data.xml

---
+++ Lecturer.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace SoftwareDesign
+{
+    class Lecturer
+    {
+        public string name;
+        public string[] subjects = new string[10];
+        //public List<string> subjects = new List<string>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,34 +7,64 @@ namespace SoftwareDesign
     class Program
     {
         public static List<Student> Students = new List<Student>();
+        public static List<Lecturer> Lecturers = new List<Lecturer>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             readData();
+            int c = Students.Count;
+            int d = Lecturers.Count;
             foreach (Student i in Students)
             {
                 Console.WriteLine(i.name + " " + i.semester);
             }
+            Console.WriteLine("Es sind " + c + " Studenten registriert.");
+            foreach (Lecturer i in Lecturers)
+            {
+                Console.WriteLine(i.name + " " + i.subjects[0] + i.subjects[1] + i.subjects[2] + i.subjects[3]);
+            }
+            Console.WriteLine("Es sind " + d + " Dozenten registriert.");
         }
 
         private static void readData()
         {
             XmlTextReader reader = new XmlTextReader("data.xml");
-
             while (reader.Read())
             {
                 if (reader.NodeType == XmlNodeType.Element)
                 {
-                    Student student = new Student();
-                    Students.Add(student);
                     switch (reader.Name)
                     {
-                        case "name":
-                            student.name = reader.ReadString();
-                            //Console.WriteLine(student.name);
+                        case "student":
+                            Student student = new Student();
+                            Students.Add(student);
+                            if (reader.HasAttributes)
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    if (reader.Name == "name")
+                                        student.name = reader.Value;
+                                    if (reader.Name == "semester")
+                                        student.semester = reader.Value;
+                                }
+                            }
                             break;
-                        case "semester":
-                            student.semester = reader.ReadString();
+                        case "lecturer":
+                            Lecturer lecturer = new Lecturer();
+                            Lecturers.Add(lecturer);
+
+                            if (reader.HasAttributes)
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    if (reader.Name == "name")
+                                        lecturer.name = reader.Value;
+                                    if (reader.Name.Contains("subject"))
+                                    {
+                                        lecturer.subjects[Int32.Parse(reader.Name.Remove(0,7))] = reader.Value;
+                                    }
+                                }
+                            }
                             break;
                     }
                 }
